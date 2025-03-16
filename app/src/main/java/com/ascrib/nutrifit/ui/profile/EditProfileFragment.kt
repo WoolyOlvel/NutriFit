88992142cc5769e9ae9bd86125a30a959c54ebf3
jesushuchin <7623@itescam.edu.mx>
2025-03-16 03:35:29 -0600

    Actualizacion V0.1.0.5.1

---
+++ app/src/main/java/com/ascrib/nutrifit/ui/profile/EditProfileFragment.kt
@@ -0,0 +1,100 @@
+package com.ascrib.nutrifit.ui.profile
+
+import com.ascrib.nutrifit.R
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.core.view.updateLayoutParams
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
+
+import com.cninfotech.swasthyedoctor.util.getStatusBarHeight
+
+import android.app.Activity
+import androidx.activity.result.ActivityResult
+
+import androidx.activity.result.contract.ActivityResultContracts.*
+import com.ascrib.nutrifit.databinding.FragmentEditProfileBinding
+
+
+class EditProfileFragment : Fragment() {
+    lateinit var binding: FragmentEditProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_edit_profile, container, false)
+
+        binding.handler = this
+        toolbarConfig()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun toolbarConfig() {
+        binding.toolbar.toolbar.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+            setMargins(0, activity?.getStatusBarHeight()!!.plus(10), 0, 0)
+        }
+
+        binding.toolbar.toolbar.title = ""
+        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar.toolbar)
+
+        (requireActivity() as AppCompatActivity).apply {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowHomeEnabled(true)
+        }
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.header_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.home_nav_graph -> {
+                        findNavController().navigateUp()
+                        true
+                    }
+
+                    R.id.action_notification -> {
+                        findNavController().navigate(R.id.global_notificationFragment)
+                        return true
+                    }
+
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    fun selectImage() {
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        takePhotoForResult.launch(gallery)
+    }
+
+    private val takePhotoForResult =
+        registerForActivityResult(StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data?.data
+                binding.imageProfile.setImageURI(intent)
+            }
+        }
+
+    fun saveProfile(){
+        findNavController().navigateUp()
+    }
+}
---
+++ app/src/main/java/com/ascrib/nutrifit/ui/profile/TimeFragment.kt
@@ -0,0 +1,108 @@
+package com.ascrib.nutrifit.ui.profile
+
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.core.view.updateLayoutParams
+import androidx.databinding.DataBindingUtil
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ascrib.nutrifit.R
+import com.ascrib.nutrifit.databinding.FragmentTimeSelectorBinding
+import com.cninfotech.swasthyedoctor.ui.patient.TimeAdapter
+import com.cninfotech.swasthyedoctor.util.getStatusBarHeight
+
+class TimeFragment : Fragment(), TimeAdapter.deleteAppointments {
+    lateinit var binding: FragmentTimeSelectorBinding
+
+    lateinit var timeAdapter: TimeAdapter
+
+    companion object times {
+        var count = 1
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_time_selector, container, false)
+
+        binding.handler = this
+
+        count = 1
+
+        toolbarConfig()
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        makeTime()
+    }
+
+    private fun toolbarConfig() {
+        binding.toolbar.toolbar.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+            setMargins(0, activity?.getStatusBarHeight()!!.plus(10), 0, 0)
+        }
+
+        binding.toolbar.toolbar.title = "Monday"
+        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar.toolbar)
+
+        (requireActivity() as AppCompatActivity).apply {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowHomeEnabled(true)
+        }
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.header_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    android.R.id.home -> {
+                        findNavController().navigateUp()
+                        true
+                    }
+
+                    R.id.action_notification -> {
+                        findNavController().navigate(R.id.global_notificationFragment)
+                        return true
+                    }
+
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    fun makeTime() {
+        timeAdapter = TimeAdapter(count,this)
+        binding.recyclerViewTime.apply {
+            adapter = timeAdapter
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        }
+    }
+
+    fun onAddClicked() {
+        count += 1
+        makeTime()
+    }
+
+    override fun onDeleteTimer() {
+        count -= 1
+        makeTime()
+    }
+}
